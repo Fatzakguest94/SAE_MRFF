6a78f39d88da2c29aec89bdc49a081b9f031933d
roland2442 <rolandissamou@gmail.com>
2026-06-02 14:48:00 +0200

    Add files via upload

--- HelloController.java
+++ HelloController.java
@@ -1,96 +1,117 @@
 package universite_paris8.iut.rissamou.sae_td;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
+import java.util.List;
+import java.util.ArrayList;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+import universite_paris8.iut.rissamou.sae_td.Terrain;
+import universite_paris8.iut.rissamou.sae_td.TerrainVue;
 
 public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
 
-    private Terrain terrain = new Terrain();
-
-    public TilePane map;
     @FXML
+    private TilePane map;
 
-    private void creerTerrain() {
-        int[][] grille = terrain.grille;
+    private universite_paris8.iut.rissamou.sae_td.TerrainVue TerrainVue;
 
-        int tailleTuile = 36;
-        map.setPrefColumns(31);
-        map.setPrefRows(19);
+    private Timeline gameLoop;
 
-        map.setPrefTileWidth(tailleTuile);
-        map.setPrefTileHeight(tailleTuile);
+    private int temps;
+    @FXML
+    private Pane panneauJeu;
 
-        Image solnoir = new Image(getClass().getResourceAsStream("solblanc.png"));
-        Image sol = new Image(getClass().getResourceAsStream("tapisrose.png"));
-        Image entrer = new Image(getClass().getResourceAsStream("entrer.jpg"));
-        Image sortie = new Image(getClass().getResourceAsStream("door.png"));
-        Image barrage5 = new Image(getClass().getResourceAsStream("barrage5.png"));
-        Image barrage6 = new Image(getClass().getResourceAsStream("barrage6.png"));
-        Image panneau8 = new Image(getClass().getResourceAsStream("panneau8.png"));
+    public Terrain terrain;
 
-        for (int i = 0; i < grille.length; i++) {
-            for (int j = 0; j < grille[i].length; j++) {
-                ImageView cases = new ImageView();
-                cases.setFitHeight(tailleTuile);
-                cases.setFitWidth(tailleTuile);
-                int t = grille[i][j];
-                switch (t) {
-                    case 0:
-                        cases.setImage(solnoir);
-                        break;
-                    case 1:
-                        cases.setImage(sol);
-                        break;
-                    case 2:
-                        cases.setImage(solnoir);
-                        break;
+    private EntiteVue entiteVue; // Déclare la vue des entités
+    private List<Enemie> listeEnnemis = new ArrayList<>();
 
-                    case 3:
-                        cases.setImage(entrer);
-                        break;
-
-                    case 4:
-                        cases.setImage(sortie);
-                        break;
-
-                    case 5:
-                        cases.setImage(barrage5);
-                        break;
-
-                    case 6:
-                        cases.setImage(barrage6);
-                        break;
-
-                    case 8:
-                        cases.setImage(panneau8);
-                        break;
-
-                    default:
-                        cases.setImage(solnoir);
-                        break;
-                }
-                map.getChildren().add(cases);
-            }
-        }
-    }
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        terrain = new Terrain();
+    public void initialize(URL location, ResourceBundle resources){
+        GestionImage.loadAssets();
+
+        TerrainVue = new TerrainVue();
+        TerrainVue.map = map;
+        // création du terrain
+        TerrainVue.creerTerrain();
+        terrain= new Terrain();
+
+        //System.out.println(terrain.grille.length);
+        //System.out.println(terrain.grille[0].length);
 
 
-        //map.setPrefColumns(21);
+        // On initialise la vue des entités en lui passant le Pane du FXML
+                entiteVue = new EntiteVue(panneauJeu);
 
-        System.out.println(terrain.grille.length);
-        System.out.println(terrain.grille[0].length);
-        creerTerrain();//terrain.grille, map);
+        initAnimation();
+
     }
+
+    @FXML
+    public void reactionBouton(ActionEvent event) {
+        faireApparaitreZombie();
+        gameLoop.play();
+        System.out.println("lancement du parcours");
+    }
+
+    public void faireApparaitreZombie() {
+        // On récupère automatiquement les coordonnées depuis la grille !
+        Point depart = terrain.trouverEntree();
+        Point arrivee = terrain.trouverSortie();
+
+        // 💡 Astuce : On place le zombie exactement sur le pixel de départ
+        Enemie zombie = new Enemie(depart.y * 36, depart.x * 36, 18, "Zombie");
+
+        // On calcule le chemin et on le donne au zombie
+        List<Point> cheminZombie = Bfs.bfs(terrain.grille, depart, arrivee);
+        zombie.setChemin(cheminZombie);
+        System.out.println("Nombre de cases trouvées par le BFS : " + cheminZombie.size());
+        listeEnnemis.add(zombie);
+        entiteVue.afficherEnnemie(zombie);
+    }
+
+    private void initAnimation() {
+        gameLoop = new Timeline();
+        temps = 0;
+        gameLoop.setCycleCount(Timeline.INDEFINITE);
+
+        KeyFrame kf = new KeyFrame(
+                Duration.seconds(0.017),
+                (ev -> {
+                    if (temps == 1000) { // J'ai augmenté un peu le temps pour te laisser voir l'animation
+                        System.out.println("fini");
+                        gameLoop.stop();
+                    } else if (temps % 5 == 0) {
+
+                        // NOUVEAU 4 : La vraie magie opère ici !
+                        // On demande à CHAQUE ennemi de la liste d'avancer un peu
+                        for (Enemie e : listeEnnemis) {
+                            e.avancer();
+                            //System.out.println("Position de l'ennemi en mémoire : X=" + e.getX() + ", Y=" + e.getY());
+                        }
+
+                        // Une fois qu'ils ont tous bougé dans le "modèle",
+                        // on dit à la vue de mettre à jour les ImageView sur l'écran
+                        entiteVue.mettreAJourAffichage();
+                    }
+                    temps++;
+                })
+        );
+        gameLoop.getKeyFrames().add(kf);
+    }
+
 
 }
