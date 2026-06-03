7a1b3631df0efbd89844c3cd98b42e51f312d171
risfa68 <757k2mbp4w@privaterelay.appleid.com>
2026-06-03 14:30:20 +0200

    Update Enemie.java

--- Enemie.java
+++ Enemie.java
@@ -1,17 +1,21 @@
-package universite_paris8.iut.rissamou.sae_td.modele;
+package universite_paris8.iut.fabdelrahim.sae.modele;
+
 
 import java.util.List;
 
 public class Enemie {
 
-    private int x;
-    private int y;
+    // Coordonnées en double pour permettre des déplacements précis avec la vitesse
+    private double x;
+    private double y;
     private double vitesse;
     private int hp;
     private int degat;
     private String identite;
+
     private List<Point> chemin;
-    private int etapeActuelle = 0;
+    private int etapeActuelle;
+    private boolean recompenseDonnee;
 
     public Enemie(int x, int y, double vitesse, String identite) {
         this.x = x;
@@ -20,34 +24,70 @@ public class Enemie {
         this.hp = 20;
         this.degat = 1;
         this.identite = identite;
-    }
-    public int getX() {
-        return this.x;
-    }
-    public int getY() {
-        return this.y;
+        this.etapeActuelle = 0;
+        this.recompenseDonnee = false;
     }
 
-    public int getHp() {
-        return hp;
-    }
-    public double getVitesse() {
-        return vitesse;
-    } 
-    public int getDegat() {
-        return degat;
+
+    public void recevoirDegats(int montant) {
+        this.hp -= montant;
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
     }
 
-    public int DegatSubi(Enemie enemie,Enemie enemie2){
 
-        int degsubi = enemie2.getDegat();
-
-        this.hp = enemie.getHp() - degsubi;
-
-        return this.hp;
+    public boolean prendreRecompense() {
+        if (this.estMort() && !this.recompenseDonnee) {
+            this.recompenseDonnee = true;
+            return true; // Donne la récompense
+        }
+        return false; // Déjà donnée ou pas encore mort
     }
-    public String getIdentite() {
-        return identite;
+
+    public void avancer() {
+        // Si pas de chemin ou si on est arrivé au bout, on s'arrête
+        if (this.chemin == null || this.etapeActuelle >= this.chemin.size()) {
+            return;
+        }
+
+        // Récupération de la case cible actuelle
+        Point caseCible = this.chemin.get(this.etapeActuelle);
+
+
+        int cibleX = caseCible.y * 36; // Utilise .y (colonne) pour l'axe X global
+        int sizeY = caseCible.x * 36;  // Utilise .x (ligne) pour l'axe Y global
+
+        // Déplacement fluide vers la cible sur l'axe X
+        if (this.x < cibleX) {
+            this.x += this.vitesse;
+            if (this.x > cibleX) this.x = cibleX;
+        } else if (this.x > cibleX) {
+            this.x -= this.vitesse;
+            if (this.x < cibleX) this.x = cibleX;
+        }
+
+        // Déplacement fluide vers la cible sur l'axe Y
+        if (this.y < sizeY) {
+            this.y += this.vitesse;
+            if (this.y > sizeY) this.y = sizeY;
+        } else if (this.y > sizeY) {
+            this.y -= this.vitesse;
+            if (this.y < sizeY) this.y = sizeY;
+        }
+
+        // On passe à la case suivante dès qu'on a atteint le pixel de destination
+        if (this.x == cibleX && this.y == sizeY) {
+            this.etapeActuelle++;
+        }
+    }
+
+    public boolean estMort() {
+        return this.hp <= 0;
+    }
+
+    public boolean estArrive() {
+        return this.chemin != null && this.etapeActuelle >= this.chemin.size();
     }
 
     public void setChemin(List<Point> chemin) {
@@ -55,51 +95,10 @@ public class Enemie {
         this.etapeActuelle = 0;
     }
 
-    public void avancer() {
-        // 1. Si on a fini le chemin, on arrête la méthode
-        if (chemin == null || etapeActuelle >= chemin.size()) {
-            return;
-        }
-
-        // 2. On calcule la position en pixels de la case visée
-        Point caseCible = chemin.get(etapeActuelle);
-        int cibleX = caseCible.y * 36;
-        int cibleY = caseCible.x * 36;
-
-        // 3. Déplacement horizontal (X)
-        if (this.x < cibleX) {
-            this.x += vitesse; // Va à droite
-        } else if (this.x > cibleX) {
-            this.x -= vitesse; // Va à gauche
-        }
-
-        // 4. Déplacement vertical (Y)
-        if (this.y < cibleY) {
-            this.y += vitesse; // Descend
-        } else if (this.y > cibleY) {
-            this.y -= vitesse; // Monte
-        }
-
-        // 5. On passe à la case suivante seulement si on est arrivé pile dessus
-        if (this.x == cibleX && this.y == cibleY) {
-            etapeActuelle++;
-        }
-    }
-    public boolean spawn(Enemie e, int [][]grille){
-        boolean spawn = true;
-        if(grille[this.x][this.y] != 3) {
-            spawn = false;
-        }
-        return spawn;
-    }
-
-    public boolean estMort() {
-        // <= 0 est plus sûr que == 0 (au cas où une tour fait beaucoup de dégâts d'un coup)
-        return this.hp <= 0;
-    }
-
-    public boolean estArrive() {
-        // Il est arrivé si son chemin n'est pas vide ET qu'il a atteint la dernière étape
-        return chemin != null && etapeActuelle >= chemin.size();
-    }
+    public int getX() { return (int) this.x; }
+    public int getY() { return (int) this.y; }
+    public int getHp() { return this.hp; }
+    public double getVitesse() { return this.vitesse; }
+    public int getDegat() { return this.degat; }
+    public String getIdentite() { return this.identite; }
 }
