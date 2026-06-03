0798c37838cb5b923d006f2b7759fd8bec9dfbe2
risfa68 <757k2mbp4w@privaterelay.appleid.com>
2026-06-03 11:15:12 +0200

    Update EntiteVue.java

--- EntiteVue.java
+++ EntiteVue.java
@@ -1,45 +1,52 @@
-package universite_paris8.iut.rissamou.sae_td;
-import javafx.fxml.FXML;
+package universite_paris8.iut.fabdelrahim.sae.vue;
+
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import universite_paris8.iut.fabdelrahim.sae.modele.Enemie;
 import java.util.HashMap;
 import java.util.Map;
 
 public class EntiteVue {
-    private Pane paneauJeu;
-    // Le dictionnaire magique qui relie un ennemi à son image à l'écran
+    private Pane panneauJeu;
     private Map<Enemie, ImageView> dictionnaireImages = new HashMap<>();
 
     public EntiteVue(Pane terrain) {
-        this.paneauJeu = terrain;
+        this.panneauJeu = terrain;
     }
 
     public void afficherEnnemie(Enemie e) {
         var image = GestionImage.getImage(e.getIdentite());
-        ImageView imageView = new ImageView(image);
+        if (image == null) return;
 
+        ImageView imageView = new ImageView(image);
         imageView.setFitWidth(36);
         imageView.setFitHeight(36);
-
         imageView.setLayoutX(e.getX());
         imageView.setLayoutY(e.getY());
 
-        paneauJeu.getChildren().add(imageView);
-
-        // On mémorise le lien !
+        panneauJeu.getChildren().add(imageView);
         dictionnaireImages.put(e, imageView);
     }
 
-    // Nouvelle méthode pour actualiser l'écran !
     public void mettreAJourAffichage() {
+        // On boucle sur le dictionnaire pour caler les images sur la position de la logique
         for (Map.Entry<Enemie, ImageView> entree : dictionnaireImages.entrySet()) {
             Enemie ennemiLogic = entree.getKey();
             ImageView imageVisuelle = entree.getValue();
 
-            // On aligne l'image sur les nouvelles coordonnées de l'objet
-            imageVisuelle.setLayoutX(ennemiLogic.getX());
-            imageVisuelle.setLayoutY(ennemiLogic.getY());
+            if (ennemiLogic.estMort()) {
+                imageVisuelle.setVisible(false); // Cache le zombie s'il est mort
+            } else {
+                imageVisuelle.setLayoutX(ennemiLogic.getX());
+                imageVisuelle.setLayoutY(ennemiLogic.getY());
+            }
         }
     }
-}
 
+    public void nettoyer() {
+        for (ImageView iv : dictionnaireImages.values()) {
+            panneauJeu.getChildren().remove(iv);
+        }
+        dictionnaireImages.clear();
+    }
+}
