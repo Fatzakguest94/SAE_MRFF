da0d59d19c1d7f9456ddf17cde39e689442156ff
Roland ISSAMOU <rissamou@iut.univ-paris8.fr>
2026-06-11 17:14:56 +0200

    jout du constructeur extends sur classe enemie/boss

--- sae/src/main/java/universite_paris8/iut/fabdelrahim/sae/modele/Zombies/Boss.java
+++ sae/src/main/java/universite_paris8/iut/fabdelrahim/sae/modele/Zombies/Boss.java
@@ -1,4 +1,7 @@
 package universite_paris8.iut.fabdelrahim.sae.modele.Zombies;
 
-public class Boss {
+public class Boss extends Enemie{
+    public Boss(int x, int y, double vitesse, int hp, int degat, String identite) {
+        super(x, y, vitesse, hp, degat, identite);
+    }
 }
