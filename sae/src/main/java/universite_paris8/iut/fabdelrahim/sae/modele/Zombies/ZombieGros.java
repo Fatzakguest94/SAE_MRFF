797f5aaceaa0acea5ab554dc0cfa6fedfdeb0b98
risfa68 <757k2mbp4w@privaterelay.appleid.com>
2026-06-17 23:31:18 +0200

    Update ZombieGros.java

--- sae/src/main/java/universite_paris8/iut/fabdelrahim/sae/modele/Zombies/ZombieGros.java
+++ sae/src/main/java/universite_paris8/iut/fabdelrahim/sae/modele/Zombies/ZombieGros.java
@@ -3,6 +3,6 @@ package universite_paris8.iut.fabdelrahim.sae.modele.Zombies;
 public class ZombieGros extends Enemie {
 
     public ZombieGros(int x, int y) {
-        super(x, y, 1, 100, 15, "ZombieGros");
+        super(x, y, 1, 140, 15, "ZombieGros");
     }
 }
